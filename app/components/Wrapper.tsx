6e6f421c1ce197340f98cd1d0571abaccfbae2b7
mrgentil <tshitshob@gmail.com.com>
2024-12-19 10:47:09 +0100

    Initial commit

---
+++ app/components/Wrapper.tsx
@@ -0,0 +1,19 @@
+﻿import Navbar from "./Navbar"
+import React from "react";
+
+type WrapperProps = {
+    children : React.ReactNode
+}
+
+const Wrapper = ({children} : WrapperProps) => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="px-5 md:px-[10%] mt-10 mb-10">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Wrapper
